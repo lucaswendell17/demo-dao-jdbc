f8f8e9eb036912bf12f3373942ae689cbc051963
Lucas Wendel <lucas.wmartins@hotmail.com>
2023-04-16 12:39:47 -0300

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,13 +2,15 @@ package application;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
-		
+		Seller obj = sellerDao.findById(3);
+		System.out.println(obj);
 	}
 
 }
